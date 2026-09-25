d1a0895ffa0c08a05437bb5e3c702791adc897cd
soleFelidae <preykerthebest26@gmail.com>
2026-09-25 11:32:55 +0800

    feat(auth): enable trustHost for custom domain and update env example

--- auth.config.ts
+++ auth.config.ts
@@ -2,6 +2,7 @@ import type { NextAuthConfig } from 'next-auth';
 import type { AppRole } from '@/types/next-auth';
 
 export const authConfig = {
+  trustHost: true,
   pages: {
     signIn: '/login',
   },
